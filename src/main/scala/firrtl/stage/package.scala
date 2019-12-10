5d09cbfda10d21b72d526f6806d0b7e19c476ff2
Schuyler Eldridge <schuyler.eldridge@ibm.com>
2019-12-10 12:48:15 -0500

    Remove firrtl.stage.phases.WriteEmitted
    This removes WriteEmitted and its associated tests. This is fully
    replaced by HowToSerialize and WriteOutputAnnotations.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@ibm.com>

--- src/main/scala/firrtl/stage/package.scala
+++ src/main/scala/firrtl/stage/package.scala
@@ -3,7 +3,6 @@
 package firrtl
 
 import firrtl.options.{OptionsView, Viewer}
-import firrtl.stage.phases.WriteEmitted
 
 /** The [[stage]] package provides an implementation of the FIRRTL compiler using the [[firrtl.options]] package. This
   * primarily consists of:
@@ -36,8 +35,6 @@ package object stage {
   }
 
   private [firrtl] implicit object FirrtlExecutionResultView extends OptionsView[FirrtlExecutionResult] {
-
-    private lazy val dummyWriteEmitted = new WriteEmitted
 
     def view(options: AnnotationSeq): FirrtlExecutionResult = {
       val fopts = Viewer[FirrtlOptions].view(options)
